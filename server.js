5a47995abd212075b1a90433327df5d998764143
jefferson <jefferson.morais2@etec.sp.gov.br>
2021-10-16 11:23:43 -0300

    add status api

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // Application
 const routes = require("./src/routes");
+const json = require("./package.json");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  console.log(data);
+  res.status(200).json({
+    name: json.name,
+    version: json.version,
+    status: json.status,
+  });
+});
 app.use("/", routes);
 
 app.listen(port, () => {
