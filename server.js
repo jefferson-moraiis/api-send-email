4a8b8d21c909d05487a2e86b401ab9685969a092
jefferson <jefferson.morais2@etec.sp.gov.br>
2021-10-16 14:42:49 -0300

    change in server.js

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const routes = require("./src/routes");
 const json = require("./package.json");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT_SERVER;
+console.log(port);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
-  console.log(data);
   res.status(200).json({
     name: json.name,
     version: json.version,
