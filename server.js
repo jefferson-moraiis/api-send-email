d308a410a35631fb1af2157796f541663ffef6a9
jefferson.morais <jefferson.morais@toroinvestimentos.com.br>
2022-01-08 18:06:42 -0300

    add cors

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const routes = require("./src/routes");
 const json = require("./package.json");
 const cors = require("cors");
 
-app.use(cors());
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
